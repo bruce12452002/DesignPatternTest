eaf61e7205b283b8a11c32e20a540ca9fd9b053f
bruce <xxx>
2019-12-25 15:57:54 +0800

    優化組合模式寫法

--- src/composite/CompositeTest.java
+++ src/composite/CompositeTest.java
@@ -1,6 +1,7 @@
 package composite;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CompositeTest {
     // 組合設計模式：使用遞迴產生樹結構
@@ -11,19 +12,37 @@ public class CompositeTest {
         var file2 = new File("file2.txt");
         folder1.addNode(file2);
 
+        var folder2 = new Folder("folder2");
+        var file3 = new File("file3.txt");
+        folder2.addNode(file3);
+        folder1.addNode(folder2);
+
         var root = new Folder("root");
         root.addNode(file1);
         root.addNode(folder1);
-        displayDetail(root);
+//        displayDetail(root);
+        displayDetail(root, 0);
+    }
+
+    private static void displayDetail(IFolder folder, int deep) {
+        for (var i = 0; i < deep; i++) {
+            System.out.print("|--");
+        }
+        folder.display();
+
+        Optional<List<IFolder>> ocn = Optional.ofNullable(folder.getChildNode());
+        ocn.ifPresent(l -> l.forEach(s -> displayDetail(s, deep + 1)));
+
+//        List<IFolder> childNode = folder.getChildNode();
+//        if (childNode != null) {
+//            childNode.forEach(s -> displayDetail(s, deep + 1));
+//        }
     }
 
     private static void displayDetail(IFolder folder) {
         folder.display();
 
-        List<IFolder> childNode = folder.getChildNode();
-        if (childNode != null) {
-            childNode.forEach(CompositeTest::displayDetail);
-        }
-
+        Optional<List<IFolder>> ocn = Optional.ofNullable(folder.getChildNode());
+        ocn.ifPresent(s -> s.forEach(CompositeTest::displayDetail));
     }
 }
