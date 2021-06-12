155fe76526e17028ca10f122260f5ed56382cdd7
bruce <bruce>
2021-06-13 02:28:33 +0800

    add reflection

--- src/singleton/Singleton2.java
+++ src/singleton/Singleton2.java
@@ -1,9 +1,25 @@
 package singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 public class Singleton2 { // 懶漢式, thread-safe
     private static Singleton2 instance;
+    private static boolean flag = false;
 
     private Singleton2() {
+        synchronized (Singleton4.class) {
+            if (!flag) {
+                flag = true;
+            } else {
+                throw new RuntimeException("don't user reflection!");
+            }
+
+//            if (instance != null) {
+//                throw new RuntimeException("don't user reflection!");
+//            }
+        }
     }
 
     public static synchronized Singleton2 getInstance() {
@@ -12,5 +28,24 @@ public class Singleton2 { // 懶漢式, thread-safe
             // return new Singleton1(); // 因為有可能一開始就進來很多執行緒，直接回傳不是 static
         }
         return instance;
+    }
+
+    // 反射測試，只有 單例 enum 才可防止反射的 new
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+//        Singleton2 instance = Singleton2.getInstance();
+//        Singleton2 instance2 = Singleton2.getInstance();
+//        System.out.println(instance);
+//        System.out.println(instance2);
+
+        Field flag = Singleton2.class.getDeclaredField("flag");
+        flag.setAccessible(true);
+
+        Constructor<Singleton2> declaredConstructor = Singleton2.class.getDeclaredConstructor();
+        declaredConstructor.setAccessible(true);
+        Singleton2 instance3 = declaredConstructor.newInstance(); // newInstance() 原碼有提示，使用 Enum 可防止反射取得實體
+        flag.set(instance3, false);
+        Singleton2 instance4 = declaredConstructor.newInstance();
+        System.out.println(instance3);
+        System.out.println(instance4);
     }
 }
--- src/singleton/Singleton6.java
+++ src/singleton/Singleton6.java
@@ -1,7 +1,24 @@
 package singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public enum Singleton6 { // enum
     INSTANCE;
 
-    public void xxx(){}
+    public void xxx() {
+    }
+
+    // 單例 Enum 可防止反射的 new
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        // Exception in thread "main" java.lang.NoSuchMethodException: singleton.Singleton6.<init>() 沒有預設建構子，使用 javap -p 居然看不出來
+        // Constructor<Singleton6> declaredConstructor = Singleton6.class.getDeclaredConstructor();
+
+        // 通過反編譯，可發現 enum 本身就是個 class，繼承了抽象的 Enum，它只有一個兩個參數的建構子，String 和 int
+        // String name, int ordinal：可以得到 INSTANCE 這個字串和第幾個，從 0 開始
+        Constructor<Singleton6> declaredConstructor = Singleton6.class.getDeclaredConstructor(String.class, int.class);
+        declaredConstructor.setAccessible(true);
+        Singleton6 instance = declaredConstructor.newInstance();
+        System.out.println(instance);
+    }
 }
