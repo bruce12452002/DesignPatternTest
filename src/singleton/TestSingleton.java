1b3674aeb278ad2f42ae2c0918032dc56349c99e
bruce <bruce>
2021-10-27 10:38:21 +0800

    update list to set

--- src/singleton/TestSingleton.java
+++ src/singleton/TestSingleton.java
@@ -1,52 +1,54 @@
 package singleton;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.CountDownLatch;
 
 public class TestSingleton {
     public static void main(String[] args) {
-        var instance1a = Singleton1.getInstance();
-        var instance1b = Singleton1.getInstance();
-        instance1a.setXxx("xxx");
-        System.out.println(instance1a.getXxx());
-        System.out.println(instance1b.getXxx());
+//        var instance1a = Singleton1.getInstance();
+//        var instance1b = Singleton1.getInstance();
+//        instance1a.setXxx("xxx");
+//        System.out.println(instance1a.getXxx());
+//        System.out.println(instance1b.getXxx());
+//
+//        var instance2a = Singleton2.getInstance();
+//        var instance2b = Singleton2.getInstance();
+//
+//        var instance3a = Singleton3.getInstance();
+//        var instance3b = Singleton3.getInstance();
+//
+//        var instance4a = Singleton4.getInstance();
+//        var instance4b = Singleton4.getInstance();
+//
+//        var instance5a = Singleton5.getInstance();
+//        var instance5b = Singleton5.getInstance();
+//
+//        var instance6a = Singleton6.INSTANCE;
+//        var instance6b = Singleton6.INSTANCE;
+//
+//        System.out.println(instance1a == instance1b);
+//        System.out.println(instance2a == instance2b);
+//        System.out.println(instance3a == instance3b);
+//        System.out.println(instance4a == instance4b);
+//        System.out.println(instance5a == instance5b);
+//        System.out.println(instance6a == instance6b);
 
-        var instance2a = Singleton2.getInstance();
-        var instance2b = Singleton2.getInstance();
 
-        var instance3a = Singleton3.getInstance();
-        var instance3b = Singleton3.getInstance();
-
-        var instance4a = Singleton4.getInstance();
-        var instance4b = Singleton4.getInstance();
-
-        var instance5a = Singleton5.getInstance();
-        var instance5b = Singleton5.getInstance();
-
-        var instance6a = Singleton6.INSTANCE;
-        var instance6b = Singleton6.INSTANCE;
-
-        System.out.println(instance1a == instance1b);
-        System.out.println(instance2a == instance2b);
-        System.out.println(instance3a == instance3b);
-        System.out.println(instance4a == instance4b);
-        System.out.println(instance5a == instance5b);
-        System.out.println(instance6a == instance6b);
-
-
-        List<Singleton4> list = new CopyOnWriteArrayList<>();
+        Set<Singleton2> set = new CopyOnWriteArraySet<>();
         CountDownLatch countDownLatch = new CountDownLatch(50);
         for (int i = 1; i <= 50; i++) {
             new Thread(() -> {
-                list.add(Singleton4.getInstance());
+                set.add(Singleton2.getInstance());
                 countDownLatch.countDown();
             }).start();
         }
 
         try {
             countDownLatch.await();
-            System.out.println(list.size());
+            System.out.println(set.size());
         } catch (InterruptedException e) {
 
         }
