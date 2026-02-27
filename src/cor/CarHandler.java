f0d489a732e692dcf568aab54be6e4a541cbc1e6
bruce <bruce>
2026-02-28 00:10:57 +0800

    add comment

--- src/cor/CarHandler.java
+++ src/cor/CarHandler.java
@@ -6,6 +6,20 @@ public abstract class CarHandler {
     public abstract void handle();
 
     public CarHandler setNext(CarHandler carHandler) {
-        return this.next = carHandler;
+        this.next = carHandler;
+        return carHandler; // 因為指派給 this.next，所以 return this.next 也行
+
+        // 可簡化成一行
+        // return this.next = carHandler;
+
+        // 注意以下寫法的問題
+        // this.next = carHandler;
+        // return this;
+        // 問題就是 return this;
+        // 當 head.setNext(body).setNext(tail);
+        // 第一次的 this 是 head，然後將 body 放到 next 裡，最後回傳 head
+        // 所以第二次的 this 是 head 不是 body，然後將 tail 放到 next 裡，最後回傳 head
+        // 最後的結果就是 head --> tail，中間的鏈都不見了
+        // 但改成 return carHandler 時，傳進來是什麼就是什麼了
     }
 }
